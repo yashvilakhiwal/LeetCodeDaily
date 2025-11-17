e386bb18ea4ade49462d0bba8d02cb9d3388ec9a
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-11-17 23:10:15 +0530

    Added

---
+++ lengthK.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int lastOneIndex = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                if (lastOneIndex != -1) {
+                    if (i - lastOneIndex - 1 < k) {
+                        return false;
+                    }
+                }
+                lastOneIndex = i;
+            }
+        }
+        return true;
+    }
+};
