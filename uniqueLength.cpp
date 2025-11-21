397f539fb99fb5efcd672dd299373bd8c62f5e90
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-11-21 23:07:03 +0530

    Added

---
+++ uniqueLength.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int n = s.length();
+        int count = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int first = -1, last = -1;
+            
+            for (int i = 0; i < n; i++) {
+                if (s[i] == c) {
+                    if (first == -1) {
+                        first = i;
+                    }
+                    last = i;
+                }
+            }
+            if (first != -1 && first < last) {
+                unordered_set<char> uniqueChars;
+                for (int i = first + 1; i < last; i++) {
+                    uniqueChars.insert(s[i]);
+                }
+                count += uniqueChars.size();
+            }
+        }
+        
+        return count;
+    }
+};
