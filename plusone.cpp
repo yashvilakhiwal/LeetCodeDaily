0a62d5b10a7a76144940d4e07f7e533dd3b1f412
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-01-01 16:12:13 +0530

    added

---
+++ plusone.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+         if(digits[n-1]!=9){
+            digits[n-1]+=1;
+            return digits;
+         }
+         for(int i=n-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }
+            else{
+                digits[i]+=1;
+                return digits;
+            }
+         }
+         digits.insert(digits.begin(),1);
+         return digits;
+    }
+};
