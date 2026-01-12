ad952623dda3ef55342fa385c13a58d8a38f25a2
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-01-12 20:28:45 +0530

    added

---
+++ minimumTime.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int totalTime=0;
+
+        for(int i=0;i<points.size()-1;i++){
+            int x1=points[i][0];
+            int y1=points[i][1];
+
+            int x2=points[i+1][0];
+            int y2=points[i+1][1];
+
+            int dx=abs(x2-x1);
+            int dy=abs(y2-y1);
+
+            totalTime+=max(dx,dy);
+        }
+        return totalTime;
+    }
+};
