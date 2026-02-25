392ed26f2506b000a39b4d6bcc5522f69d6ec2f5
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-02-25 21:37:06 +0530

    added

---
+++ sortByBits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            int ca = __builtin_popcount(a), cb = __builtin_popcount(b);
+            return ca != cb ? ca < cb : a < b;
+        });
+        return arr;
+    }
+};
