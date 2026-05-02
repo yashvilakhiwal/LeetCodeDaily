d507e209876724a722a8835b7510c147d6ece22c
yashvilakhiwal <yashvilakhiwal@gmail.com>
2026-05-02 22:43:28 +0530

    added

---
+++ rotatedDigits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        vector<int>t(n+1,-1);
+        t[0]=0;
+        int count=0;
+        for(int i=0;i<=n;i++)
+        {
+            int remain=t[i/10];
+            if(remain==2)
+            {
+                t[i]=2;
+                continue;
+            }
+            int d=i%10;
+            int digit_check;
+            if(d==0||d==1||d==8)
+            {
+                digit_check=0;
+            }
+            else if(d==2||d==5||d==6||d==9)
+            {
+                digit_check=1;
+            }
+            else{
+                t[i]=2;
+                continue;
+            }
+            if(remain==0 && digit_check==0)
+            {
+                t[i]=0;
+            }
+            else{
+                t[i]=1;
+            }
+            if(t[i]==1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
